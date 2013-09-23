39b0696fc40b61080c1f231fa64321cce8362e78
smilesol85 <smilesol85@gmail.com>
2013-09-23 22:09:27 +0900

    motify

--- dev/reset_code/js/reset_code.js
+++ dev/reset_code/js/reset_code.js
@@ -1,194 +1,202 @@
 var reset = reset || {};
-var nRmAttr = 0, nRmTag = 0;
 
-reset.code = function(){
+reset.Code = function(){
     this.init();
 };
 
-reset.code.prototype = {
+reset.Code.prototype = {
     init : function(){
+
         this._initVals();
         this._setEvents();
     },
     _initVals : function(){
-        this.btnRmAttr = $('.add_rm_attr');
-        this.btnRmTag = $('.add_rm_tag');
-        this.btnReset = $('.btn_reset');
+        this.nRmAttr = 0;
+        this.nRmTag = 0;
+        this.welBtnRmAttr = $('.add_rm_attr');
+        this.welBtnRmTag = $('.add_rm_tag');
+        this.welBtnReset = $('.btn_reset');
     },
     _setCheck : function(){
-        this.fHref = $('#reset_href').is(':checked');
-        this.fScript = $('#rm_script_tag').is(':checked');
-        this.fIframe = $('#rm_iframe_tag').is(':checked');
-        this.fTarget = $('#rm_target_blank').is(':checked');
-        this.fOnclick = $('#rm_onclick').is(':checked');
-        this.fOnerror = $('#rm_onerror').is(':checked');
-        this.fOnload = $('#rm_onload').is(':checked');
-        this.fnocr = $('#rm_nocr').is(':checked');
-        this.fUnderbar = $('#rm_underbar').is(':checked');
+        this.welHref = $('#reset_href').is(':checked');
+        this.welScript = $('#rm_script_tag').is(':checked');
+        this.welIframe = $('#rm_iframe_tag').is(':checked');
+        this.welTarget = $('#rm_target_blank').is(':checked');
+        this.welOnclick = $('#rm_onclick').is(':checked');
+        this.welOnerror = $('#rm_onerror').is(':checked');
+        this.welOnload = $('#rm_onload').is(':checked');
+        this.welNocr = $('#rm_nocr').is(':checked');
+        this.welUnderbar = $('#rm_underbar').is(':checked');
     },
     _setReg : function(){
-        //this.regScript = /<script.*[\r\n].*[\r\n]<\/script>/gi;
-        //this.regTarget = /target=["'][^"']+["']/gi;
-        //this.regOnclick = /onclick=["'].*["']/gi;
-        //this.regOnerror = /onerror=["'].*["']/gi;
-        //this.regOnload = /onload=["'].*["']/gi;
-        //this.regNocr = /nocr=["'].*["']/gi;
+        //this.rxScript = /<script.*[\r\n].*[\r\n]<\/script>/gi;
+        //this.rxTarget = /target=["'][^"']+["']/gi;
+        //this.rxOnclick = /onclick=["'].*["']/gi;
+        //this.rxOnerror = /onerror=["'].*["']/gi;
+        //this.rxOnload = /onload=["'].*["']/gi;
+        //this.rxNocr = /nocr=["'].*["']/gi;
 
-        this.regHtml = /html/gi;
-        this.regHead = /head>/gi;
-        this.regBody = /body/gi;
-        this.regLink = /link/gi;
+        this.rxHtml = /html/gi;
+        this.rxHead = /head>/gi;
+        this.rxBody = /body/gi;
+        this.rxLink = /link/gi;
 
-        this.regHtml2 = /htmlS/gi;
-        this.regHead2 = /headS>/gi;
-        this.regBody2 = /bodyS/gi;
-        this.regLink2 = /LinkS/gi;
+        this.rxHtml2 = /htmlS/gi;
+        this.rxHead2 = /headS>/gi;
+        this.rxBody2 = /bodyS/gi;
+        this.rxLink2 = /LinkS/gi;
         
-        this.regLink3 = /<\/link>/gi;
-        this.regHref = /href=["'][^"']+["']/gi;
-        this.regScript = /<script/gi;
-        this.regScript2 = /<\/script/gi;
-        this.regNoScript = /<noscript/gi;
-        this.regNoScript2 = /<\/noscript/gi;
-        this.regTarget = /target=/gi;
-        this.regOnclick = /onclick=/gi;
-        this.regOnerror = /onerror=/gi;
-        this.regOnload = /onload=/gi;
-        this.regNocr = /nocr=/gi;
+        this.rxLink3 = /<\/link>/gi;
+        this.rxHref = /href=["'][^"']+["']/gi;
+        this.rxScript = /<script/gi;
+        this.rxScript2 = /<\/script/gi;
+        this.rxNoScript = /<noscript/gi;
+        this.rxNoScript2 = /<\/noscript/gi;
+        this.rxTarget = /target=/gi;
+        this.rxOnclick = /onclick=/gi;
+        this.rxOnerror = /onerror=/gi;
+        this.rxOnload = /onload=/gi;
+        this.rxNocr = /nocr=/gi;
 
-        this.regCdata = /\/\/<\!\[CDATA\[/gi;
-        this.regCdata2 = /\/\/\]\]>>/gi;
+        this.rxCdata = /\/\/<\!\[CDATA\[/gi;
+        this.rxCdata2 = /\/\/\]\]>>/gi;
 
         /* space */
-        this.regUnderbar = /_[^blank]\w+/gi;
-        this.regBlank = /\s[>]/gi;
-        this.regBlank2 = /\s["]/gi;
-        this.regBlank3 = /\s[']/gi;
+        this.rxUnderbar = /_[^blank]\w+/gi;
+        this.rxBlank = /\s[>]/gi;
+        this.rxBlank2 = /\s["]/gi;
+        this.rxBlank3 = /\s[']/gi;
+
+        //this.rxLineBlank = /\r\n|\n\n/gi;
     },
 
     // 속성 추가
     _setEleRmAttr : function(sValue){
-        if(!sValue){
-        }else{
-            $('.user_rm_attr').append('<li><input type="checkbox" id="rm_user_attr'+nRmAttr+'" checked="checked" disabled="disabled"><label for="rm_user_attr'+nRmAttr+'">'+sValue+'</label></li>');
-            nRmAttr++;
+        if(sValue !== '' && sValue !== undefined && sValue !== null){
+            $('.user_rm_attr').append('<li><input type="checkbox" id="rm_user_attr'+this.nRmAttr+'" checked="checked" disabled="disabled"><label for="rm_user_attr'+this.nRmAttr+'">'+sValue+'</label></li>');
+            this.nRmAttr++;
         }
     },
 
     // 태그 추가
     _setEleRmTag : function(sValue){
-        if(!sValue){
-        }else{
-            $('.user_rm_tag').append('<li><input type="checkbox" id="rm_user_tag'+nRmTag+'" checked="checked" disabled="disabled"><label for="rm_user_tag'+nRmTag+'">'+sValue+'</label></li>');
-            nRmTag++;
+        if(sValue !== '' && sValue !== undefined && sValue !== null){
+            $('.user_rm_tag').append('<li><input type="checkbox" id="rm_user_tag'+this.nRmTag+'" checked="checked" disabled="disabled"><label for="rm_user_tag'+this.nRmTag+'">'+sValue+'</label></li>');
+            this.nRmTag++;
         }
     },
+
+    // 변환
+    _replace : function(){
+        var sReplace = '';
+
+        /* html, head, body, link, cdata
+        * 각 태그에 임시로 S 붙여서 제거되지 않게 방지
+        */
+        sReplace = $('.before').val().replace(this.rxHtml,'htmlS').replace(this.rxHead,'headS>').replace(this.rxBody,'bodyS').replace(this.rxLink,'linkS').replace(this.rxCdata,'').replace(this.rxCdata2,'');
+        $('.before').val(sReplace);
+
+        // script
+        this.welScript ? sReplace = $('.before').val().replace(this.rxScript,'<div data-rm="rmself"').replace(this.rxScript2,'</div').replace(this.rxNoScript,'<div data-rm="rmself"').replace(this.rxNoScript2,'</div') : sReplace = $('.before').val();
+        $('.before').val(sReplace);
+        
+        // target
+        this.welTarget ? sReplace = $('.before').val().replace(this.rxTarget,'data-rmself=') : sReplace = $('.before').val();
+        $('.before').val(sReplace);
+
+        // onclick
+        this.welOnclick ? sReplace = $('.before').val().replace(this.rxOnclick,'data-rmself=') : sReplace = $('.before').val();
+        $('.before').val(sReplace);
+
+        // onerror
+        this.welOnerror ? sReplace = $('.before').val().replace(this.rxOnerror,'data-rmself=') : sReplace = $('.before').val();
+        $('.before').val(sReplace);
+
+        // onload
+        this.welOnload ? sReplace = $('.before').val().replace(this.rxOnload,'data-rmself=') : sReplace = $('.before').val();
+        $('.before').val(sReplace);
+
+        // nocr
+        this.welNocr ? sReplace = $('.before').val().replace(this.rxNocr,'data-rmself=') : sReplace = $('.before').val();
+        $('.before').val(sReplace);
+
+        // underbar class & id
+        this.welUnderbar ? sReplace = $('.before').val().replace(this.rxUnderbar,'') : sReplace = $('.before').val();
+        $('.before').val(sReplace);
+
+        // 공백 제거
+        sReplace = $('.before').val().replace(this.rxBlank,'>').replace(this.rxBlank2,'"').replace(this.rxBlank3,'\'');
+        $('.before').val(sReplace);
+
+        // 추가 속성 제거
+        var nAttr = $('.user_rm_attr li').length;
+        for(var i = 0; i < nAttr; i++){
+            var sRmAttr = $('#rm_user_attr'+i+' + label').text();
+            var rxRmAttr = new RegExp(sRmAttr+'=','gi');
+            sReplace = $('.before').val().replace(rxRmAttr,'data-rmself=');
+            $('.before').val(sReplace);
+        }
+
+        // 추가 태그 제거
+        var nTag = $('.user_rm_tag li').length;
+        for(var j = 0; j < nTag; j++){
+            var sRmTag = $('#rm_user_tag'+j+' + label').text();
+            var rxRmTag = new RegExp('<'+sRmTag,'gi');
+            sReplace = $('.before').val().replace(rxRmTag,'<'+sRmTag+' data-rm="rmself"');
+            $('.before').val(sReplace);
+        }
+
+        /* 특정 영역에 삽입
+        * a태그 href 초기화
+        * iframe 제거
+        * 언더바 속성(클래스 및 아이디) 제거
+        * data-rm 속성을 가진 태그 전부 제거
+        * data-rmself 속성 전부 제거
+        * 다시 textarea 에 삽입
+        */
+        $('.render').html(sReplace);
+        this.welHref ? sReplace = $('.render a').attr('href','#') : '';
+        this.welIframe ? sReplace = $('iframe').remove() : '';
+        /*
+        if(this.welUnderbar){
+            //$('.render *').attr();
+            $('.render *').each(function(){
+                $(this).attr('class');
+            });
+        }
+        */
+        $('[data-rm]').remove();
+        $('.render *').removeAttr('data-rmself');
+        sReplace = $('.render').html();
+        $('.before').val(sReplace);
+
+        /* html, head, body, link
+        * 각 태그 원복
+        */
+        sReplace = $('.before').val().replace(this.rxHtml2,'html').replace(this.rxHead2,'head>').replace(this.rxBody2,'body').replace(this.rxLink2,'link').replace(this.rxLink3,'');
+        $('.before').val(sReplace);
+    },
+
     _setEvents : function(){
         var oSelf = this;
 
         // prompt : 제거하고 싶은 속성 입력
-        this.btnRmAttr.on('click',function(){
+        this.welBtnRmAttr.on('click',function(){
             var sRemove = prompt('제거하고 싶은 속성 입력 : ','');
             oSelf._setEleRmAttr(sRemove);
         });
 
         // prompt : 제거하고 싶은 태그 입력
-        this.btnRmTag.on('click',function(){
+        this.welBtnRmTag.on('click',function(){
             var sRemove = prompt('제거하고 싶은 태그 입력 : ','');
             oSelf._setEleRmTag(sRemove);
         });
 
         // 체크 여부 확인 후 코드 수정
-        this.btnReset.on('click',function(){
-            var sReplace = '', getHref = '', getSplitHref = [];
+        this.welBtnReset.on('click',function(){
             oSelf._setCheck();
             oSelf._setReg();
-
-            /* html, head, body, link, cdata
-            * 각 태그에 임시로 S 붙여서 제거되지 않게 방지
-            */
-            sReplace = $('.before').val().replace(oSelf.regHtml,'htmlS').replace(oSelf.regHead,'headS>').replace(oSelf.regBody,'bodyS').replace(oSelf.regLink,'linkS').replace(oSelf.regCdata,'').replace(oSelf.regCdata2,'');
-            $('.before').val(sReplace);
-
-            // script
-            oSelf.fScript ? sReplace = $('.before').val().replace(oSelf.regScript,'<div data-rm="rmself"').replace(oSelf.regScript2,'</div').replace(oSelf.regNoScript,'<div data-rm="rmself"').replace(oSelf.regNoScript2,'</div') : sReplace = $('.before').val();
-            $('.before').val(sReplace);
-            
-            // target
-            oSelf.fTarget ? sReplace = $('.before').val().replace(oSelf.regTarget,'data-rmself=') : sReplace = $('.before').val();
-            $('.before').val(sReplace);
-
-            // onclick
-            oSelf.fOnclick ? sReplace = $('.before').val().replace(oSelf.regOnclick,'data-rmself=') : sReplace = $('.before').val();
-            $('.before').val(sReplace);
-
-            // onerror
-            oSelf.fOnerror ? sReplace = $('.before').val().replace(oSelf.regOnerror,'data-rmself=') : sReplace = $('.before').val();
-            $('.before').val(sReplace);
-
-            // onload
-            oSelf.fOnload ? sReplace = $('.before').val().replace(oSelf.regOnload,'data-rmself=') : sReplace = $('.before').val();
-            $('.before').val(sReplace);
-
-            // nocr
-            oSelf.fnocr ? sReplace = $('.before').val().replace(oSelf.regNocr,'data-rmself=') : sReplace = $('.before').val();
-            $('.before').val(sReplace);
-
-            // underbar class & id
-            oSelf.fUnderbar ? sReplace = $('.before').val().replace(oSelf.regUnderbar,'') : sReplace = $('.before').val();
-            $('.before').val(sReplace);
-
-            // 공백 제거
-            sReplace = $('.before').val().replace(oSelf.regBlank,'>').replace(oSelf.regBlank2,'"').replace(oSelf.regBlank3,'\'');
-            $('.before').val(sReplace);
-
-            // 추가 속성 제거
-            var nAttr = $('.user_rm_attr li').length;
-            for(var i = 0; i < nAttr; i++){
-                var sRmAttr = $('#rm_user_attr'+i+' + label').text();
-                var regRmAttr = new RegExp(sRmAttr+'=','gi');
-                sReplace = $('.before').val().replace(regRmAttr,'data-rmself=');
-                $('.before').val(sReplace);
-            }
-
-            // 추가 태그 제거
-            var nTag = $('.user_rm_tag li').length;
-            for(var j = 0; j < nTag; j++){
-                var sRmTag = $('#rm_user_tag'+j+' + label').text();
-                var regRmTag = new RegExp('<'+sRmTag,'gi');
-                sReplace = $('.before').val().replace(regRmTag,'<'+sRmTag+' data-rm="rmself"');
-                $('.before').val(sReplace);
-            }
-
-            /* 특정 영역에 삽입
-            * a태그 href 초기화
-            * iframe 제거
-            * 언더바 속성(클래스 및 아이디) 제거
-            * data-rm 속성을 가진 태그 전부 제거
-            * data-rmself 속성 전부 제거
-            * 다시 textarea 에 삽입
-            */
-            $('.render').html(sReplace);
-            oSelf.fHref ? sReplace = $('.render a').attr('href','#') : '';
-            oSelf.fIframe ? sReplace = $('iframe').remove() : '';
-            /*
-            if(oSelf.fUnderbar){
-                //$('.render *').attr();
-                $('.render *').each(function(){
-                    $(this).attr('class');
-                });
-            }
-            */
-            $('[data-rm]').remove();
-            $('.render *').removeAttr('data-rmself');
-            sReplace = $('.render').html();
-            $('.before').val(sReplace);
-
-            /* html, head, body, link
-            * 각 태그 원복
-            */
-            sReplace = $('.before').val().replace(oSelf.regHtml2,'html').replace(oSelf.regHead2,'head>').replace(oSelf.regBody2,'body').replace(oSelf.regLink2,'link').replace(oSelf.regLink3,'');
-            $('.before').val(sReplace);
+            oSelf._replace();
         });
     }
 };
