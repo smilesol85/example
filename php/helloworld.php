efaa812615f49ac1eac8407472e67ece18384af8
HanSol <smilesol85@gmail.com>
2013-10-16 22:02:48 +0900

    add file

---
+++ php/helloworld.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ko">
+    <head>
+        <title>hello world</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta http-equiv="Content-Script-Type" content="text/javascript">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, target-densitydpi=medium-dpi">
+        <link rel="stylesheet" type="text/css" href="../vCommon/vCommon.css">
+    </head>
+    <body>
+        <div id="wrap">
+            <script>var titHeader = 'hello world';</script>
+            <script src="../vCommon/header.js"></script>
+            <section>
+                echo "Hello world";
+                <?php
+                echo "Hello world";
+                ?>
+            </section>
+            <script src="../vCommon/footer.js"></script>
+        </div>
+    </body>
+</html>
