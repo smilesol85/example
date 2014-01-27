8ac7c4686aa5bcc9ba3a9394ac92de137729a396
smilesol85 <smilesol85.gmail.com>
2014-01-27 15:25:32 +0900

    blink

--- backup/dev/check_device_browser/js/check_device_browser.js
+++ backup/dev/check_device_browser/js/check_device_browser.js
@@ -1,3 +1,17 @@
+// test code
+//var isie=(/msie/i).test(sUserAgent); //ie
+//var isie6=(/msie 6/i).test(sUserAgent); //ie 6
+//var isie7=(/msie 7/i).test(sUserAgent); //ie 7
+//var isie8=(/msie 8/i).test(sUserAgent); //ie 8
+//var isie9=(/msie 9/i).test(sUserAgent); //ie 9
+//var isfirefox=(/firefox/i).test(sUserAgent); //firefox
+//var isapple=(/applewebkit/i).test(sUserAgent); //safari,chrome
+//var isopera=(/opera/i).test(sUserAgent); //opera
+//var isios=(/(ipod|iphone|ipad)/i).test(sUserAgent);//ios
+//var isipad=(/(ipad)/i).test(sUserAgent);//ipad
+//var isandroid=(/android/i).test(sUserAgent);//android
+//if(isios || isipad) enableFlash = false;
+
 var checkUserAgent = checkUserAgent || {};
                 
 checkUserAgent = function(){
@@ -9,39 +23,40 @@ checkUserAgent.prototype = {
     
     init : function(){
         alert(this.sUserAgent);
-        this.addArrDevice();
-        this.checkDevice();
+        this.addArrUserAgent();
+        this.checkUserAgent();
     },
     
-    addArrDevice : function(){
-        this.arrDevice = [];
+    addArrUserAgent : function(){
+        this.arrUserAgent = [];
         
         // window
-        this.arrDevice.push('windows');
+        this.arrUserAgent.push('windows');
         
         // apple
-        this.arrDevice.push('iphone', 'ipod', 'ipad', 'macintosh');
+        this.arrUserAgent.push('iphone', 'ipod', 'ipad', 'macintosh');
         
         // android
-        this.arrDevice.push('android');
+        this.arrUserAgent.push('android');
         
         // blackberrt
-        this.arrDevice.push('blackberrt');
+        this.arrUserAgent.push('blackberrt');
         
         // nexus
-        this.arrDevice.push('nexus');
+        this.arrUserAgent.push('nexus');
         
         // kindle
-        this.arrDevice.push('kindle');
+        this.arrUserAgent.push('kindle');
         
         // browser
-        this.arrDevice.push('chrome', 'safari', 'applewebkit', 'firefox', 'msie', 'msie 6', 'msie 7', 'msie 8', 'msie 9', 'msie 10');
+        this.arrUserAgent.push('chrome', 'safari', 'applewebkit', 'firefox', 'msie', 'msie 6', 'msie 7', 'msie 8', 'msie 9', 'msie 10');
     },
     
-    checkDevice : function(){
-        for(var nDevice in this.arrDevice){
-            if(this.sUserAgent.match(this.arrDevice[nDevice])){
-                $('body').addClass(this.arrDevice[nDevice]);
+    checkUserAgent : function(){
+        $('body').attr('data-useragent', this.sUserAgent);
+        for(var nUserAgent in this.arrUserAgent){
+            if(this.sUserAgent.match(this.arrUserAgent[nUserAgent])){
+                $('body').addClass(this.arrUserAgent[nUserAgent]);
                 alert($('body').attr('class'));
             }
         }
@@ -49,17 +64,3 @@ checkUserAgent.prototype = {
 };
 
 var oCheckUserAgent = new checkUserAgent();
-
-
-//                var isie=(/msie/i).test(sUserAgent); //ie
-//                var isie6=(/msie 6/i).test(sUserAgent); //ie 6
-//                var isie7=(/msie 7/i).test(sUserAgent); //ie 7
-//                var isie8=(/msie 8/i).test(sUserAgent); //ie 8
-//                var isie9=(/msie 9/i).test(sUserAgent); //ie 9
-//                var isfirefox=(/firefox/i).test(sUserAgent); //firefox
-//                var isapple=(/applewebkit/i).test(sUserAgent); //safari,chrome
-//                var isopera=(/opera/i).test(sUserAgent); //opera
-//                var isios=(/(ipod|iphone|ipad)/i).test(sUserAgent);//ios
-//                var isipad=(/(ipad)/i).test(sUserAgent);//ipad
-//                var isandroid=(/android/i).test(sUserAgent);//android
-//                if(isios || isipad) enableFlash = false;
