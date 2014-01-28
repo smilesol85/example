643ce38d9f5d79ca33ae3f827d43b0bbcae10d56
smilesol85 <smilesol85.gmail.com>
2014-01-28 14:33:58 +0900

    modiefied orientation js code

--- dev/orientation/js/orientation.js
+++ dev/orientation/js/orientation.js
@@ -12,21 +12,23 @@ orient.check.prototype = {
     },
     
     checkOrient : function(){
+        var sWrite = '';
         $(window).bind('orientationchange', function(e){
             switch(window.orientation){
                 case 0: 
                     // document.getElementById("orient_css").href = "css/iphone_portrait.css";
-                    this.elContent.html += '0 <br>';
+                    sWrite = '0';
                     break;
                 case -90: 
                     // document.getElementById("orient_css").href = "css/iphone_landscape.css";
-                    this.elContent.html += '-90 <br>';
+                    sWrite = '-90';
                     break;
                 case 90: 
                     // document.getElementById("orient_css").href = "css/iphone_landscape.css";
-                    this.elContent.html += '+90 <br>';
+                    sWrite = '+90';
                     break;
             }
+            this.elContent.html(sWrite);
         });
     }
 };
