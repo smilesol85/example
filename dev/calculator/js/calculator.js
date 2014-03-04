f6a661614aa4197076f521d182cadbd6f20b9cf0
smilesol85 <smilesol85.gmail.com>
2014-03-04 15:51:54 +0900

    http module and add calculator file

---
+++ dev/calculator/js/calculator.js
@@ -0,0 +1,13 @@
+var calculator = calculator || {};
+
+calculator.active = function(){
+    this.init();
+};
+
+calculator.active.prototype = {
+    init : function(){
+    
+    }
+};
+
+var oCalculator = new calculator.active();
