62ac71f246e05d59cc5f321b92f4e82b7ebabdbd
smilesol85 <smilesol85.gmail.com>
2014-03-04 21:57:03 +0900

    calculator dev

--- dev/calculator/js/calculator.js
+++ dev/calculator/js/calculator.js
@@ -6,7 +6,44 @@ calculator.active = function(){
 
 calculator.active.prototype = {
     init : function(){
+        this.elInput();
+        this.addition();
+        this._setEvent();
+    },
     
+    elInput : function(){
+        this.nInput = document.querySelector('input');
+        this.btnAddition = document.querySelector('#addition');
+        this.btnSubtraction = document.querySelector('#subtraction');
+        this.btnMultiplication = document.querySelector('#multiplication');
+        this.btnDivision = document.querySelector('#division');
+        this.btnRoot = document.querySelector('#root');
+    },
+    
+    addition : function(){
+        
+    },
+    
+    subtraction : function(){
+        
+    },
+    
+    multiplication : function(){
+    
+    },
+    
+    division : function(){
+    
+    },
+    
+    root : function(){
+    
+    },
+    
+    _setEvent : function(){
+        this.btnAddition.addEventListener('click', function(){
+            alert(this.nInput);
+        });
     }
 };
 
