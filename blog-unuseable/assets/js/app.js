9cde89153afe6d82cbca52e26bf941ddfaf3e45e
Hansol Park <hansol.park@linecorp.com>
2014-05-09 19:32:06 +0900

    clean

--- blog-unuseable/assets/js/app.js
+++ blog-unuseable/assets/js/app.js
@@ -6,12 +6,12 @@ $( document ).ready(function() {
 	/* Secondary contact links */
 	var scontacts = $('#contact-list-secondary');
 	var contact_list = $('#contact-list');
-	
+
     /*
 	scontacts.hide();
-	
+
 	contact_list.mouseenter(function(){ scontacts.slideDown(); });
-	
+
 	contact_list.mouseleave(function(){ scontacts.slideUp(); });
     */
 
