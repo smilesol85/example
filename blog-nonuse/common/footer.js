cea2ef501f921f12b5e3d9471fc4afca01d974db
Hansol Park <hansol.park@linecorp.com>
2014-08-08 10:23:55 +0900

    clean

--- blog-nonuse/common/footer.js
+++
@@ -1,7 +0,0 @@
-var sFooter = '';
-sFooter += '<div id="footer">';
-sFooter += '<div class="container">';
-sFooter += '<p class="muted credit">smilesol85@gmail.com</p>';
-sFooter += '</div>';
-sFooter += '</div>';
-document.write(sFooter);
