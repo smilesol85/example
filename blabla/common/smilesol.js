e7bb1d39eacf1c2a47dd0653bd67a688ecd811b7
Hansol Park <hansol.park@linecorp.com>
2014-09-05 17:25:44 +0900

    update

--- blabla/common/smilesol.js
+++ blabla/common/smilesol.js
@@ -1,3 +1,13 @@
 (function () {
-    console.log('hello');
+
+	// check image load
+	var elLoad = $('.check-el-load');
+	var nLoad = 0;
+
+	elLoad.each(function() {
+		$(this).on('load', function() {
+			++nLoad;
+			console.log('이미지 '+nLoad+'개 로드');
+		});
+	});
 })();
