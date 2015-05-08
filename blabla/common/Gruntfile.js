bca40f8113820a4a5320ff75fe8081a53b1aa6b2
Hansol Park <hansol.park@linecorp.com>
2015-05-08 11:58:39 +0900

    reset

--- blabla/common/Gruntfile.js
+++ blabla/common/Gruntfile.js
@@ -1,253 +1,215 @@
-/*
-	grunt-contrib-compress
-	grunt-contrib-watch  // 특정 파일들 감시하다가, 변경되면 지정한 작업 수행
-	grunt-contrib-compass  // sass/scss를 지원
-	grunt-contrib-uglify
-	grunt-contrib-cssmin
-	grunt-ngmin
-	grunt-contrib-concat
-	grunt-encode-images  // 이미지 base64로 변환
-	grunt-smushit  // 이미지 압축
-	grunt-contrib-imagemin  // 이미지 압축
-*/
-
 module.exports = function(grunt) {
-
-	// 프로젝트 환경설정.
+    var appConfig = {
+        app: 'src',
+        dist: 'dist',
+//        webview: '../..'
+    },
+    packageConfig = grunt.file.readJSON('package.json');
 	grunt.initConfig({
-	pkg: grunt.file.readJSON('package.json'),
-		uglify: {
-		//      options: {
-		//        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-		//      },
-		  build: {
-			src: 'js/*.js',
-			dest: 'js-min/uglify.min.js'
-		  }
-		},
 
-		smushit: {
-			mygroup: {
-			  src: 'img',
-			  dest: 'img-min'
-			}
-		},
+        prj: appConfig,
+        pkg: packageConfig,
 
-		// Imagemin
-		imagemin: {
-			files: {
-				expand: true,
-//				cwd: 'app/pc/',
-				src: ['img/*.{png, jpg, gif}'],
-				dest: 'img-min'
-			}
-
-//			pc: {
-//				files: [
-//					{
-//						expand: true,
-//						cwd: 'app/pc/',
-//						src: ['img/*.{png, jpg, gif}'],
-//						dest: 'dist/pc/'
-//					}
-//				]
-//			},
-//			mobile: {
-//				files: [
-//					{
-//						expand: true,
-//						cwd: 'app/mobile/',
-//						src: ['img/*.{png, jpg, gif}'],
-//						dest: 'dist/mobile/'
-//					}
-//				]
-//			}
-		}
-	});
-
-	// "uglify" task를 지원하는 플러그인 로드.
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-smushit');
-	grunt.loadNpmTasks('grunt-contrib-imagemin');
-
-	// Default task(s).
-	grunt.registerTask('js-min', ['uglify']);
-	grunt.registerTask('img-min', ['smushit']);
-	grunt.registerTask('img-min2', ['imagemin']);
-
-};
-
-module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
 		watch: {
 			compass: {
 				files: ['**/*.scss'],
-				tasks: ['compass:dev', 'cssmin:minify']
+				tasks: ['compass:dev', 'cssmin:minify', 'autoprefixer', 'clean:dev']
 			},
-			scripts: {
-                files: ['common/smilesol.js'],
-                tasks: ['uglify']
+			includes: {
+				files: ['<%= prj.app %>/html/tpls/*.html', '<%= prj.app %>/html/includes/*.html'],
+				tasks: ['includes']
+			},
+            jsc: {
+                files: ['<%= prj.app %>/js/*.js', '!<%= prj.app %>/js/*.min.js'],
+                tasks: ['clean:dev', 'uglify']
             }
 		},
-		compass: {
+
+        compass: {
 			dev: {
 				options: {
-//					sassDir: ['app/scss'],
-//					cssDir: ['app/css'],
-					sassDir: ['common'],
-					cssDir: ['common'],
-					environment: 'development'
-				}
-			},
-			prod: {
-				options: {
-//					sassDir: ['app/scss'],
-//					cssDir: ['app/css'],
-					sassDir: ['common'],
-					cssDir: ['common'],
-					environment: 'production'
+					noLineComments: true,
+					sassDir: ['<%= prj.app %>/scss'],
+					cssDir: ['<%= prj.app %>/scss'],
+					environment: 'development',
+					imagesDir: '<%= prj.app %>/img-sprites-src',
+					generatedImagesPath: '<%= prj.app %>/img',
+					httpGeneratedImagesPath: '../img'
 				}
 			}
 		},
-		cssmin: {
+
+        cssmin: {
+            option: {
+                shorthandCompacting: false
+            },
 			minify: {
 				files: {
-					'common/stylesheet.min.css': [
-						'common/stylesheet.css'
+					'<%= prj.app %>/css/all.min.css': [
+						'<%= prj.app %>/scss/*.css'
 					]
 				}
 			}
 		},
-		ngmin: {
-			apps: {
-				src: [
-					'app/js/app.js',
-					'app/js/app.controller.js',
-					'app/js/app.service.js',
-					'app/js/app.factory.js'
-					'common/smilesol.js'
-				],
-				dest: 'common/smilesol.min.js'
-			}
-		},
-		 uglify: {
-			options: {
-				banner: '/* <%= grunt.template.today("yyyy-mm-dd") %> */ ' //파일의 맨처음 붙는 banner 설정
-			},
-			build: {
-				src: 'public/build/result.js', //uglify할 대상 설정
-				dest: 'public/build/result.min.js' //uglify 결과 파일 설정
-			}
-        },
-//		uglify: {
-//			dist: {
-//				files: {
-//					'common/smilesol.min.js': 'common/smilesol.js'
-//				}
-//			}
-//			all: {
-//				files: {
-//					'app/js/jquery.min.js': [
-//						'bower_components/jquery/dist/jquery.js'
-//					],
-//					'app/js/angular.min.js': [
-//						'bower_components/angular/angular.js',
-//						'bower_components/angular-animate/angular-animate.js',
-//						'bower_components/angular-cookies/angular-cookies.js',
-//						'bower_components/angular-route/angular-route.js',
-//						'bower_components/angular-sanitize/angular-sanitize.js',
-//						'bower_components/angular-touch/angular-touch.js',
-//						'bower_components/angular-websql/angular-websql.js',
-//						'bower_components/angular-local-storage/angular-local-storage.js',
-//						'bower_components/angular-ui-router/release/angular-ui-router.js',
-//						'bower_components/angular-ui-router.stateHelper/statehelper.js',
-//						'bower_components/angular-ui-utils/ui-utils.js',
-//						'bower_components/ngInfiniteScroll/build/ng-infinite-scroll.js'
-//					],
-//					'app/js/fastclick.min.js': [
-//						'bower_components/fastclick/lib/fastclick.js'
-//					],
-//					'app/js/package.min.js': [
-//						'app/js/jquery.min.js',
-//						'app/js/angular.min.js',
-//						'app/js/fastclick.min.js'
-//					]
-//				}
-//			}
-//		},
-		concat:{
-            basic: {
-                src: ['public/js/common/util.js', 'public/js/app.js', 'public/js/lib/*.js', 'public/js/ctrl/*.js'], //concat 타겟 설정(앞에서부터 순서대로 합쳐진다.)
-                dest: 'public/build/result.js' //concat 결과 파일
+
+        autoprefixer: {
+            options: {
+                browsers: [
+                    'Android 2.3',
+                    'Android >= 4',
+                    'Chrome >= 20',
+                    'Firefox >= 24', // Firefox 24 is the latest ESR
+                    'Explorer >= 6',
+                    'iOS >= 5',
+                    'Opera >= 12',
+                    'Safari >= 6'
+                ]
+            },
+            dev: {
+                src: '<%= prj.app %>/css/all.min.css',
+                dest: '<%= prj.app %>/css/stylesheet.min.css'
             }
         },
-		compress: {
-			main: {
-//				options: {
-//					mode: 'gzip'
-//				},
-//				files: [{
-//					expand: true,
-//					cwd: 'app/js',
-//					src: ['**/*.min.js'],
-//					dest: 'app/js/',
-//					ext: '.min.js.gz'
-//				}]
+
+        includes: {
+            dev: {
+				cwd: '<%= prj.app %>/html/tpls',
+				src: ['*.html'],
+				dest: '<%= prj.app %>/html/',
+				options: {
+					flatten: true,
+					includePath: '<%= prj.app %>/html/includes/'
+				}
 			}
 		},
 
-		smushit: {
-			mygroup: {
-			  src: 'img',
-			  dest: 'img-min'
+        clean: {
+            options: {
+                force: true,
+                dot: true
+            },
+            dev: ['<%= prj.app %>/js/*.min.js', '<%= prj.app %>/scss/*.css'],
+			build: ['<%= prj.dist %>/'],
+            webview: ['<%= prj.webview %>/'],
+            webIndex: ['../markup/webview/LINE-Music/*.html'],
+		},
+
+        imagemin: {
+			build: {
+                expand: true,
+                cwd: '<%= prj.app %>/img/',
+                src: ['**/*.{png, jpg, gif}'],
+                dest: '<%= prj.dist %>/img'
+            }
+		},
+
+		encodeImages: {
+			build: {
+				files: [{
+					expand: true,
+					cwd: '<%= prj.dist %>/css/',
+					src: '**/giftshop.css',
+					dest: '<%= prj.dist %>/css/'
+				}]
 			}
 		},
 
-		// Imagemin
-		imagemin: {
+		prettify: {
+			options: {
+				"indent": 1,
+				"indent_char": "\t",
+				"indent_scripts": "normal",
+				"wrap_line_length": 0,
+				"brace_style": "collapse",
+				"preserve_newlines": true,
+				"max_preserve_newlines": 1,
+				"unformatted": [
+					"a",
+					"code",
+					"pre"
+				]
+			},
 			files: {
 				expand: true,
-//				cwd: 'app/pc/',
-				src: ['img/*.{png, jpg, gif}'],
-				dest: 'img-min'
+				cwd: '<%= prj.dist %>/',
+				src: ['html/*.html'],
+				dest: '<%= prj.dist %>/'
 			}
+		},
 
-//			pc: {
-//				files: [
-//					{
-//						expand: true,
-//						cwd: 'app/pc/',
-//						src: ['img/*.{png, jpg, gif}'],
-//						dest: 'dist/pc/'
-//					}
-//				]
-//			},
-//			mobile: {
-//				files: [
-//					{
-//						expand: true,
-//						cwd: 'app/mobile/',
-//						src: ['img/*.{png, jpg, gif}'],
-//						dest: 'dist/mobile/'
-//					}
-//				]
-//			}
+		validation: {
+			options: {
+				reset: grunt.option('reset') || false,
+				stoponerror: false,
+				relaxerror: ['Bad value X-UA-Compatible for attribute http-equiv on element meta.']
+			},
+			files: {
+				src: ['<%= prj.app %>/html/*.html']
+			}
+		},
+
+		copy: {
+			build: {
+				files: [
+					{
+						expand:true,
+						cwd: '<%= prj.app %>/',
+						src: [
+                            'html/*.html',
+                            'css/stylesheet.min.css',
+                            'js/*.js',
+                            'img/**/*.jpg',
+                            'img/**/*.gif'
+                        ],
+						dest: '<%= prj.dist %>/'
+					}
+				]
+			},
+            webview: {
+				files: [
+					{
+						expand:true,
+						cwd: '<%= prj.dist %>/',
+                        src: ['**'],
+						dest: '<%= prj.webview %>/'
+					}
+				]
+			},
+            webIndex: {
+                files: [
+					{
+						expand:true,
+						cwd: '',
+                        src: ['index.html'],
+						dest: '../markup/webview/LINE-Music/'
+					}
+				]
+            }
+		},
+
+		uglify: {
+            dev: {
+                src: '<%= prj.app %>/js/*.js',
+                dest: '<%= prj.app %>/js/uglify.min.js'
+            }
 		}
 	});
-
 	grunt.loadNpmTasks('grunt-contrib-compress');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-compass');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-ngmin');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-smushit');
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-encode-images');
+	grunt.loadNpmTasks('grunt-pngmin');
+	grunt.loadNpmTasks('grunt-prettify');
+	grunt.loadNpmTasks('grunt-includes');
+	grunt.loadNpmTasks('grunt-html-validation');
+    grunt.loadNpmTasks('grunt-autoprefixer');
 
-	grunt.registerTask('default', ['concat', 'compass:dev', 'cssmin', 'uglify', 'watch']);
-	grunt.registerTask('build', ['compass:dev', 'cssmin', 'uglify']);
-	grunt.registerTask('img-min', ['smushit']);
-	grunt.registerTask('img-min2', ['imagemin']);
+	grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['clean:build', 'imagemin', 'copy:build', 'prettify']);
+    grunt.registerTask('webview', ['clean:webview', 'clean:webIndex', 'copy:webview', 'copy:webIndex']);
 };
